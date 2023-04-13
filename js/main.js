c7edc3d203c738ae7e1c995c16988aee47ef439a
KoltsovDmitriy <121514804+KoltsovDmitriy@users.noreply.github.com>
2023-04-13 07:44:16 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,17 @@
+(function(){
+    const header = document.querySelector('.header');
+    window.onscroll = () =>{
+        if(window.pageYOffset > 70){
+            header.classList.add('header_active');            
+        }else {
+            header.classList.remove('header_active'); 
+        }
+    }    
+}())
+window.onload = function () {
+    document.body.classList.add('loaded_hiding');
+    window.setTimeout(function () {
+      document.body.classList.add('loaded');
+      document.body.classList.remove('loaded_hiding');
+    }, 500);
+  }
